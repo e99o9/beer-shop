f50ea2c0703d8858829301b632fa5dc735787b1c
e99o9 <localdealer@yandex.ru>
2025-07-29 19:50:52 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: 20 }}>
+      <h1>Пивная Лавка</h1>
+      <p>Сайт в разработке. QR-код Kaspi доступен в полной версии.</p>
+      <img src="/kaspi-qr.png" alt="Kaspi QR" style={{ width: 200 }} />
+    </div>
+  )
+}
